ea51e67475e894891c479b9eed2a5c7064a4d768
namkcits <132549602+namkcits@users.noreply.github.com>
2023-05-04 00:37:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+ 
+Import os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
