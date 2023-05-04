850c2a63960ad86fa7be87db1f637b4296f28eb0
namkcits <132549602+namkcits@users.noreply.github.com>
2023-05-04 01:05:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-Import numpy as np
+import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
