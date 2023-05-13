7edda29610f485edd19ccb6146b16f3af4ec824f
namkcits <132549602+namkcits@users.noreply.github.com>
2023-05-13 13:50:32 -0700

    Update quantum_iris_app.py

--- quantum_iris_app.py
+++ quantum_iris_app.py
@@ -1,9 +1,6 @@
-import numpy as np
-import sympy
-import tensorflow as tf
+numpy as np
+
+ensorflow as tf
 import tensorflow_quantum as tfq
-import streamlit as st
+import streamlit as st Lynn
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import cirq
