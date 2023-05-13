7e6fadae044f317c2677d68a5d4d590453d6a5ed
namkcits <132549602+namkcits@users.noreply.github.com>
2023-05-13 13:24:22 -0700

    Update Requerments.py

--- Requerments.py
+++ Requerments.py
@@ -1,7 +1,7 @@
-pip install tensorflow==2.6.0
-pip install tensorflow-quantum==0.6.1
-pip install cirq==0.11.1
-pip install numpy==1.19.5
-pip install scikit-learn==0.24.2
-pip install sympy==1.7.1
-pip install streamlit==1.0.0
+!pip install tensorflow==2.6.0
+!pip install tensorflow-quantum==0.6.1
+!pip install cirq==0.11.1
+!pip install numpy==1.19.5
+!pip install scikit-learn==0.24.2
+!pip install sympy==1.7.1
+!pip install streamlit==1.0.0
