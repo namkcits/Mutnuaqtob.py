79da3064c84e2e90d46bcbfe0e896f1823979ace
namkcits <132549602+namkcits@users.noreply.github.com>
2023-05-13 13:40:47 -0700

    Create quantum_iris_app.py

---
+++ quantum_iris_app.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sympy
+import tensorflow as tf
+import tensorflow_quantum as tfq
+import streamlit as st
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import cirq
